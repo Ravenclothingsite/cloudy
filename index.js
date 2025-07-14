b6bebcf866687653d2f225b77b4b534bcff411ec
CLOUDYdrip <sameershaikh102023@gmail.com>
2025-07-15 01:14:05 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
-      <h1 style={{ fontSize: '3rem' }}>CLOUDY</h1>
-      <p>Streetwear for dreamers. Made for Gen Z.</p>
-    </div>
-  );
-}
